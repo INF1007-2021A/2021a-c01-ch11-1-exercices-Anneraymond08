cf14affc4fe45c5e6bb4fc682cbf481766b425f1
Anneraymond08 <90001135+Anneraymond08@users.noreply.github.com>
2021-11-19 11:40:27 -0500

    pas fini

--- game.py
+++ game.py
@@ -13,13 +13,20 @@ import utils
 class Weapon:
 	"""
 	Une arme dans le jeu.
-
 	:param name: Le nom de l'arme
 	:param power: Le niveau d'attaque
 	:param min_level: Le niveau minimal pour l'utiliser
 	"""
+	def __init__(self, name, power, min_level):
+		self.name = name
+		self.power = power
+		self.min_level = min_level
 
-	UNARMED_POWER = 20
+	def make_unarmed(self):
+		self.name = "Unarmed"
+		self.power = 20
+		self.min_level = 0
+
 
 
 class Character:
@@ -32,7 +39,17 @@ class Character:
 	:param defense: Le niveau de défense du personnage
 	:param level: Le niveau d'expérience du personnage
 	"""
+	def __init__(self, name, max_hp, attack, defense, level, weapon, hp):
+		self.name = name
+		self.max_hp = max_hp
+		self.attack = attack
+		self.defense = defense
+		self.level = level
+		self.weapon = weapon
+		self.hp = hp
 
+	def compute_damage(self, personnage_b):
+		self.
 
 def deal_damage(attacker, defender):
 	# TODO: Calculer dégâts
